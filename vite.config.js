4c5e0f2f67f28bd12b4c36f0fd5a9514aa12125a
WilliamAraujo <w67543210@gmail.com>
2025-09-15 22:51:52 -0300

    imagens optmizadas

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,24 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import { ViteImageOptimizer } from "vite-plugin-image-optimizer";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    ViteImageOptimizer({
+      png: { quality: 80 },
+      jpeg: { quality: 75 },
+      webp: { quality: 80 },
+      avif: { quality: 70 },
+      svg: {
+        plugins: [
+          { name: "removeViewBox", active: false },
+          { name: "sortAttrs" },
+        ],
+      },
+    }),
+    react(),
+    tailwindcss(),
+  ],
 });
